7163059fb70eca098931c5bda5cfce016ec8c0e8
Divyanshi Pant <divyanshi2936@gmail.com>
2026-07-22 23:13:58 +0530

    Time: 4 ms (89.67%), Space: 8.9 MB (75.16%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -0,0 +1,45 @@
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+
+    if (n == 0)
+        return "";
+
+    int start = 0;
+    int maxLen = 1;
+
+    for (int i = 0; i < n; i++) {
+
+        // Odd length palindrome
+        int left = i;
+        int right = i;
+
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+
+        // Even length palindrome
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    char *ans = (char *)malloc((maxLen + 1) * sizeof(char));
+
+    strncpy(ans, s + start, maxLen);
+    ans[maxLen] = '\0';
+
+    return ans;
+}
