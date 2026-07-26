a55be63e03662b10267d73d7b14d27a5c4ce346d
Divyanshi Pant <divyanshi2936@gmail.com>
2026-07-26 23:56:28 +0530

    Time: 0 ms (100%), Space: 10.3 MB (4.87%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
@@ -0,0 +1,28 @@
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n)
+{
+    struct ListNode dummy;
+    dummy.next = head;
+
+    struct ListNode *fast = &dummy;
+    struct ListNode *slow = &dummy;
+
+    
+    for (int i = 0; i <= n; i++)
+    {
+        fast = fast->next;
+    }
+
+    
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    
+    struct ListNode *temp = slow->next;
+    slow->next = temp->next;
+    free(temp);
+
+    return dummy.next;
+}
