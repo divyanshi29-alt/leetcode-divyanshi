6885c57f67dddecbb5cf9090d8ccf04cc5a69245
Divyanshi Pant <divyanshi2936@gmail.com>
2026-07-29 23:33:15 +0530

    Time: 4 ms (37.39%), Space: 14.1 MB (59.41%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+
+    void backtrack(vector<int>& candidates, int target, int index) {
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (index == candidates.size() || target < 0)
+            return;
+
+        temp.push_back(candidates[index]);
+        backtrack(candidates, target - candidates[index], index);
+        temp.pop_back();
+
+        backtrack(candidates, target, index + 1);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        backtrack(candidates, target, 0);
+        return ans;
+    }
+};
