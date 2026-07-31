993cbcb272d669bdd526a4a0c606f74d02469bb2
Divyanshi Pant <divyanshi2936@gmail.com>
2026-07-31 23:24:11 +0530

    Time: 8 ms (28.53%), Space: 15.9 MB (7.57%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        unordered_set<int> used;
+
+        for (int i = idx; i < nums.size(); i++) {
+            if (used.count(nums[i])) continue;
+            used.insert(nums[i]);
+
+            swap(nums[idx], nums[i]);
+            backtrack(nums, idx + 1);
+            swap(nums[idx], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        backtrack(nums, 0);
+        return ans;
+    }
+};
