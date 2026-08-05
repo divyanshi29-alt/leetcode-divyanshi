f0e89a10e5a9c873cd4e29f08533607a324a6940
Divyanshi Pant <divyanshi2936@gmail.com>
2026-08-05 23:21:41 +0530

    Time: 0 ms (100%), Space: 11 MB (29.29%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* partition(struct ListNode* head, int x) {
+    struct ListNode lessDummy, greaterDummy;
+    struct ListNode *less = &lessDummy, *greater = &greaterDummy;
+
+    less->next = NULL;
+    greater->next = NULL;
+
+    while (head) {
+        if (head->val < x) {
+            less->next = head;
+            less = less->next;
+        } else {
+            greater->next = head;
+            greater = greater->next;
+        }
+        head = head->next;
+    }
+
+    greater->next = NULL;
+    less->next = greaterDummy.next;
+
+    return lessDummy.next;
+}
